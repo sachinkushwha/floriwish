477c1d1d42961abb93c608c277d23f52a28d7fa5
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2026-02-14 15:45:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://floriwish-9a2o.vercel.app",
     credentials: true
 }));
 
